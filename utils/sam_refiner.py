611aae79b93cdb8883f36217b6d3d4fcc809068f
yara-yk2104889 <yarah24.k@gmail.com>
2026-04-15 11:05:55 +0300

    edits

--- utils/sam_refiner.py
+++ utils/sam_refiner.py
@@ -44,7 +44,7 @@ class SAMRefiner:
         refined = []
         for img, logit in zip(images, unet_logits):
             refined.append(self._refine_single(img, logit))
-        return torch.stack(refined, dim=0)
+        return torch.stack(refined, dim=0).to(unet_logits.device)
 
     def _refine_single(
         self,
